c307f035539a41a9fe2832028c68b40a3681f22a
Nikita Bolshagin <bolshagin.nikita@yandex.ru>
2018-10-23 14:26:55 +0500

    Initial commit

---
+++ constants.py
@@ -0,0 +1,12 @@
+URL_VISIT = 'https://olympia.olympiaperm.ru:1443/visit.php'
+URL_SCHEDULE = 'https://olympiaperm.ru/clients/raspisanie/big-pool/'
+
+PROXY = {'https': 'socks5://82.196.10.131:1080'}
+
+TOKEN = ''
+
+CURR_TZ = 'Asia/Yekaterinburg'
+
+UPD_DELAY = 5 * 60
+DATA_PATH = ''
+PIC_PATH = ''
